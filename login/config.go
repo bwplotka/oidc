daaa534c5319508b400d1f5c93275fc732b3f615
Bartek Plotka <bwplotka@gmail.com>
2017-05-26 14:49:51 +0100

    Fixed parsing config.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- login/config.go
+++ login/config.go
@@ -13,16 +13,16 @@ type Config struct {
 	// first on the OIDC server. It can point to localhost. E.g http://127.0.0.1 -> redirectURL: http://127.0.0.1/callback
 	// This is also and address that HTTP's callback server will listen on. Bind Address must include port. You can specify 0 if your
 	// OIDC provider support wildcard on port (almost all server does NOT).
-	BindAddress string `yaml:"address"`
+	BindAddress string `json:"address"`
 
 	// Canonical URL for Provider that will be the target issuer that this server authenticate End Uusers against.
-	Provider string `yaml:"provider"`
+	Provider string `json:"provider"`
 
-	ClientID     string `yaml:"client_id"`
-	ClientSecret string `yaml:"secret"`
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"secret"`
 
-	Scopes     []string `yaml:"scopes"`
-	NonceCheck bool     `yaml:"include_nonce"`
+	Scopes     []string `json:"scopes"`
+	NonceCheck bool     `json:"include_nonce"`
 }
 
 // ConfigFromYaml parses config from yaml file.
